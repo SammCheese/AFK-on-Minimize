d7860c153ce7b01bdcbef0ed0d737e9e1b248032
Samm Cheese <sammcheese.github@gmail.com>
2021-10-17 20:07:43 +0200

    No more Powercord & Settings now apply immediately

--- components/settings.tsx
+++ components/settings.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/display-name */ // Pls shut the fuck up
 import { ErrorBoundary } from '@components';
 import { makeLazy, wrapSettings } from '@util';
 import { DNGetter, React } from '@webpack';
@@ -21,58 +22,69 @@ const statusOptions = [
   }
 ];
 
+const settings = Astra.settings.get('AFK-on-exit');
+
 const SettingsView = makeLazy({
   promise: () => {
 
-    const settings = Astra.settings.get('AFK-on-exit');
-    const { FormSection, FormItem, Markdown, FormDivider, SwitchItem } = DNGetter;
-    const { SelectInput } = require('powercord/components/settings');
-    const restoreSetting = settings.get('restoreStatus', true);
-    console.log(restoreSetting);
-
-    const SettingsView = React.memo(({} : { openingStatus : string, closingStatus : string, restoreStatus : boolean}) : React.ReactElement<typeof FormSection> => (
+    const { FormSection, FormItem, Markdown, FormDivider, SwitchItem, SelectTempWrapper } = DNGetter;
+    
+    
+    const SettingsView = React.memo(({} : { openingStatus : string, closingStatus : string, restoreStatus : boolean}) : React.ReactElement<FormSection> => (
+      <div>
+        <FormSection title='AFK on Exit' tag='h1'>
+          <FormItem>
+            <Markdown>
+              Customize your Status on given Events here
+            </Markdown>
+          </FormItem>
+        </FormSection>
+        <FormDivider />
+        <br/>
         <div>
-          <FormSection title='AFK on Exit' tag='h1'>
-            <FormItem>
-              <Markdown>
-                Customize your Status on given Events here
-              </Markdown>
-            </FormItem>
-          </FormSection>
-          <FormDivider />
+          <SwitchItem
+            onChange={React.useCallback((value: boolean): void => settings.set('restoreStatus', value), [])}
+            value={settings.get('restoreStatus', true)}
+          >
+            Restore Status after opening Discord
+          </SwitchItem>
           <br/>
-          <div>
-            <SwitchItem
-              onChange={React.useCallback((value: boolean): void => settings.set('restoreStatus', value), [])}
-              value={settings.get('restoreStatus', true)}
-            >
-              Restore Status after opening Discord
-            </SwitchItem>
-            <SelectInput
-              onChange={React.useCallback((value: string): void => settings.set('openingStatus', value), [])}
-              options={statusOptions.map(status => ({
-                label: status.name,
-                value: status.id,
-              }))}
-              value={settings.get('openingStatus', 'online').value}
-              disabled={!!restoreSetting}
-            >
-              Status to be set when Opening Discord (Disable Restore Status)
-          </SelectInput>
-          <SelectInput
-              onChange={React.useCallback((value: string): void => settings.set('closingStatus', value), [])}
-              options={statusOptions.map(status => ({
-                label: status.name,
-                value: status.id,
-              }))}
-              value={settings.get('closingStatus', 'idle').value}
-              
-            >
-              Status to be set when closing Discord
-          </SelectInput>
-          </div>
+          <FormItem>
+            <Markdown>
+              Status to be set when Opening Discord (Disable Restore Status):
+            </Markdown>
+          </FormItem>
+          <SelectTempWrapper
+            onChange={React.useCallback((value: string): void => settings.set('openingStatus', value), [])}
+            options={statusOptions.map(status => ({
+              label: status.name,
+              value: status.id
+            }))}
+            value={settings.get('openingStatus', 'online').value}
+            disabled={settings.get('restoreStatus', true)}
+          >
+            Status to be set when Opening Discord (Disable Restore Status)
+          </SelectTempWrapper>
+          <br/>
+          <FormItem>
+            <Markdown>
+              Status to be set when Closing Discord
+            </Markdown>
+          </FormItem>
+          <SelectTempWrapper
+            onChange={React.useCallback((value: string): void => settings.set('closingStatus', value), [])}
+            options={statusOptions.map(status => ({
+              label: status.name,
+              value: status.id
+            }))}
+            value={settings.get('closingStatus', 'idle').value}
+            
+          >
+            Status to be set when closing Discord
+          </SelectTempWrapper>
         </div>
-      ));
+      </div>
+    ));
     return Promise.resolve(wrapSettings(settings, SettingsView));
   }
 });
@@ -82,7 +94,7 @@ export class SettingsErrorBoundary extends ErrorBoundary {
     props.label = 'AFK on Exit Setting Panel';
     super(props);
   }
-
+  
   renderChildren(): React.ReactElement<typeof SettingsView> {
     return <SettingsView />;
   }
