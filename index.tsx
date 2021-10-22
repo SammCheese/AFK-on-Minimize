43273c636f4d32dc4a00af2119107766fb6e6629
Samm Cheese <sammcheese.github@gmail.com>
2021-10-22 14:02:53 +0200

    Fixing  some stuff

--- index.tsx
+++ index.tsx
@@ -4,7 +4,7 @@ import { SettingsErrorBoundary } from './components/settings';
 import { debounce } from 'lodash';
 
 const status = getModule(['updateRemoteSettings']);
-const dStatus = getModule(['getStatus']);
+const statusStore = getModule([ 'isMobileOnline' ], false);
 const currentUser = getModule(['getCurrentUser']).getCurrentUser().id;
 const settings = Astra.settings.get('AFK-on-exit');
 
@@ -31,21 +31,19 @@ export default class dndOnClose extends UPlugin {
 
   cumIntoClient() : void {
     const restoreStatus = settings.get('restoreStatus', true);
-    console.log('Something has changed');
-
+    
     if (restoreStatus && document.visibilityState === 'hidden' && check === false) {
-      prevDStatus = dStatus.getStatus(currentUser);
+      prevDStatus = statusStore.getStatus(currentUser);
+      console.log('[AFK-on-exit] Restoring Status...');
     }
     if (document.visibilityState === 'hidden' && prevStatus !== 'hidden') {
       prevStatus = 'hidden';
       check = true;
-      status.updateRemoteSettings({ status: settings.get('closingStatus', 'idle').value });
-      console.log('Changing status...');
+      status.updateRemoteSettings({ status: settings.get('closingStatus', 'idle').value || 'idle' });
     } else if (document.visibilityState === 'visible' && prevStatus === 'hidden') {
       prevStatus = 'visible';
       check = false;
-      status.updateRemoteSettings({ status: `${restoreStatus ? prevDStatus : settings.get('openingStatus', 'online').value}` });
-      console.log('Changing status...');
+      status.updateRemoteSettings({ status: `${restoreStatus ? prevDStatus : settings.get('openingStatus', 'online').value}` || 'online' });
     }
   }
   
